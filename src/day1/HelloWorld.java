a447692260c3010fec1984272aab724e84f43b46
Irina Tulus <irinagku@gmail.com>
2021-04-24 16:52:56 -0400

    more

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Adding GitHub Now");
+        System.out.println("more commit");
     }
+
 }
